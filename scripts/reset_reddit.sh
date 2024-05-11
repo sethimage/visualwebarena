469abc2d174740f35edeb7800a3820b8a4b6c947
Chen Wu <henrychenwu98@gmail.com>
2024-05-11 18:33:36 +0000

    Update reset

--- scripts/reset_reddit.sh
+++ scripts/reset_reddit.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Define variables
-CONTAINER_NAME="forum"
-
-docker stop $CONTAINER_NAME
-docker rm $(docker ps -a | grep $CONTAINER_NAME | awk '{print $1}')
-docker run --name $CONTAINER_NAME -p 9999:80 -p 9998:5432 -d postmill-populated-exposed-withimg:v3
+sudo docker stop forum
+sudo docker rm $(sudo docker ps -a | grep forum | awk '{print $1}')
+sudo docker run --name forum -p 9999:80 -p 9998:5432 -d postmill-populated-exposed-withimg
 # wait ~15 secs for all services to start
 sleep 15
