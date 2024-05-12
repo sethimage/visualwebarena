de375c03290febf1c8ec42912276a28d1a7bea54
Chen Wu (吴尘) <henrychenwu@cmu.edu>
2024-05-12 15:47:47 -0400

    Update reset_reddit.sh

--- scripts/reset_reddit.sh
+++ scripts/reset_reddit.sh
@@ -5,3 +5,5 @@ sudo docker rm $(sudo docker ps -a | grep forum | awk '{print $1}')
 sudo docker run --name forum -p 9999:80 -p 9998:5432 -d postmill-populated-exposed-withimg
 # wait ~15 secs for all services to start
 sleep 15
+
+bash prepare.sh
