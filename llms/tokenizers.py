55711f579762fc4c900ee54c94c17a149cc3c37e
Chen Wu <henrychenwu98@gmail.com>
2024-05-13 18:53:00 +0000

    Update tokenizer

--- llms/tokenizers.py
+++ llms/tokenizers.py
@@ -7,6 +7,8 @@ from transformers import LlamaTokenizer  # type: ignore
 class Tokenizer(object):
     def __init__(self, provider: str, model_name: str) -> None:
         if provider == "openai":
+            if model_name.startswith("gpt-4o"):
+                model_name = "gpt-4-"  # TODO: temporary hack
             self.tokenizer = tiktoken.encoding_for_model(model_name)
         elif provider == "huggingface":
             self.tokenizer = LlamaTokenizer.from_pretrained(model_name)
